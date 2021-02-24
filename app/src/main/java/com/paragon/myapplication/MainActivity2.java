a60ee7a516fcc4eba88d861f14b04b9c581e34eb
Hridoy <hridoykrisnadas@gmail.com>
2021-02-24 14:20:16 +0600

    First

--- app/src/main/java/com/paragon/myapplication/MainActivity2.java
+++ app/src/main/java/com/paragon/myapplication/MainActivity2.java
@@ -38,7 +38,7 @@ public class MainActivity2 extends AppCompatActivity {
         CountrySpinner = findViewById(R.id.CountrySpinnerID);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_spinner_item, getCountry());
+                this, android.R.layout.simple_list_item_single_choice, getCountry());
 
         CountrySpinner.setAdapter(arrayAdapter);
 
